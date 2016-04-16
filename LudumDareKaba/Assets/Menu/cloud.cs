fd7db70ed3135b9509bd8f1bb06baff8aeb01512
Victor Gouet <Victor@MacBook-Pro-de-Victor.local>
2016-04-16 14:36:03 +0200

    menu begin and cloud

---
+++ LudumDareKaba/Assets/Menu/cloud.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cloud : MonoBehaviour {
+
+	public Rigidbody2D rb;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		rb.velocity = Vector2.right * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
