191ed7d5a322322fcc685a97c4d6468be5b665d7
Victor Gouet <Victor@MacBook-Pro-de-Victor.local>
2016-04-16 11:01:42 +0200

    metamorth

---
+++ LudumDareKaba/Assets/MetaMorph.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MetaMorph : MonoBehaviour {
+
+	//public
+	public Rigidbody2D rb;
+	void Start() {
+		rb = GetComponent<Rigidbody2D>();
+		rb.velocity = Vector2.right;
+	}
+
+	void FixedUpdate() {
+		if (Input.GetButtonDown("Jump"))
+			rb.velocity = new Vector3(0, 10, 0);
+	}
+
+	// Use this for initialization
+//	void Start () {
+//		
+//	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
