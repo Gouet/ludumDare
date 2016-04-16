7ea03075190d056d165dac864ce7bdadbe3f6ece
sabalt <matthieu.tavernier@epitech.eu>
2016-04-16 15:39:23 +0200

    destructor

---
+++ LudumDareKaba/Assets/destructor.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destructor : MonoBehaviour {
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        Debug.Log("collision name = " + coll.gameObject.name);
+        Destroy(coll.gameObject);
+    }
+}
