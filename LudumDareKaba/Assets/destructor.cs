b3d224e8c6a4d46944214b4336aa9c92261578cb
sabalt <matthieu.tavernier@epitech.eu>
2016-04-16 19:02:36 +0200

    4 spawners

--- LudumDareKaba/Assets/destructor.cs
+++ LudumDareKaba/Assets/destructor.cs
@@ -2,9 +2,8 @@
 using System.Collections;
 
 public class destructor : MonoBehaviour {
-    void OnCollisionEnter2D(Collision2D coll)
+    void OnTriggerEnter2D(Collider2D coll)
     {
-        Debug.Log("collision name = " + coll.gameObject.name);
         Destroy(coll.gameObject);
     }
 }
