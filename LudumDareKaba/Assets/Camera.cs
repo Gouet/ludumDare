186f893a67f3cc654da783bc567bb9a30a14f949
sabalt <matthieu.tavernier@epitech.eu>
2016-04-17 17:13:00 +0200

    lock resolution

---
+++ LudumDareKaba/Assets/Camera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Screen.SetResolution(1980, 1080, true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
